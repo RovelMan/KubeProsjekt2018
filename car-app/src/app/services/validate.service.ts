03ac0bb07ddabe3e27bed7145cecdb9f9207edad
gravlaks <32542379+gravlaks@users.noreply.github.com>
2018-01-28 16:53:55 +0100

    pulling form navjots-google-api

--- car-app/src/app/services/validate.service.ts
+++ car-app/src/app/services/validate.service.ts
@@ -23,7 +23,6 @@ export class ValidateService {
     } else {
       return true;
     }
-<<<<<<< HEAD
   }
 
   //may be undefined even though it is there
@@ -47,7 +46,4 @@ export class ValidateService {
     }   
       
   }
-=======
-  }  
->>>>>>> 075e58537b1cbef7070ee7bb68c297ad169d934b
 }
