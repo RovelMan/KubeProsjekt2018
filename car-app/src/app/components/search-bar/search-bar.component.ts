7355f1bfd7e39615e2ee2bf33d795ceb0ca7eeec
Tor Arne <torarne1996@hotmail.no>
2018-02-01 21:52:45 +0100

    fixing torstein-bug

--- car-app/src/app/components/search-bar/search-bar.component.ts
+++ car-app/src/app/components/search-bar/search-bar.component.ts
@@ -11,13 +11,13 @@ declare var google;
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-  searchFromV: string;
+  searchFromV: string = "";
+  searchFromInput: string = "";
   @ViewChild("searchFrom") searchFromElementRef: ElementRef;
   @ViewChild("searchTo") searchToElementRef: ElementRef;
   @Output() inputChanged: EventEmitter<any> = new EventEmitter<any>();
   @Input() searchFromP: any;
-  searchFromInput: string = "";
-
+  
   constructor(
     private mapsAPILoader: MapsAPILoader,
     private ngZone: NgZone
@@ -51,56 +51,3 @@ export class SearchBarComponent implements OnInit {
     }, 0.1);
   }
 }
-
-
-
-/*
-
-import { Component, OnInit, ElementRef, NgZone, ViewChild, EventEmitter, Output } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { FlashMessagesService } from 'angular2-flash-messages';
-import { } from 'googlemaps';
-import { MapsAPILoader } from '@agm/core';
-declare var google;
-
-export class SearchBarComponent implements OnInit {
-  searchFromV: string;
-  searchToV: string;
-  fromText: string;
-  @ViewChild("searchFrom") searchFromElementRef: ElementRef;
-  @ViewChild("searchTo") searchToElementRef: ElementRef;
-
-  constructor(
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
-  ) { }
-
-  ngOnInit() {
-    this.mapsAPILoader.load().then(() => {
-      let autocompleteOne = new google.maps.places.Autocomplete(this.searchFromElementRef.nativeElement, {
-        types: ["geocode"]
-      });
-      let autocompleteTwo = new google.maps.places.Autocomplete(this.searchToElementRef.nativeElement, {
-        types: ["geocode"]
-      });
-    });
-  }
-
-
-  private delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  private fromBtn() {
-    setTimeout(() => {
-      this.searchFromV = this.searchFromElementRef.nativeElement.value;
-      console.log(this.searchFromV);
-    }, 0.1);
-  }
-
-  private searchDestination(searchFrom: string, searchTo: string) {
-    console.log("2");
-  }
-}
-
-*/
--- car-app/src/app/components/search-trips/search-trips.component.ts
+++ car-app/src/app/components/search-trips/search-trips.component.ts
@@ -24,26 +24,3 @@ export class SearchTripsComponent implements OnInit {
   }
 
 }
-
-/*
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-
-@Component({
-  selector: 'app-search-trips',
-  templateUrl: './search-trips.component.html',
-  styleUrls: ['./search-trips.component.css']
-})
-export class SearchTripsComponent implements OnInit {
-
-  searchFromRoute: string;
-
-  constructor(private route: ActivatedRoute) { }
-  ngOnInit() {
-    this.searchFromRoute = this.route.snapshot.params['sendTest'];
-    console.log(this.searchFromRoute);
-  }
-
-}
-
-*/
