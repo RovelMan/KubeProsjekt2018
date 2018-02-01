d88430eafe084a14592276a8dab9b16bc3e7be32
Tor Arne <torarne1996@hotmail.no>
2018-02-01 11:26:09 +0100

    Testing

--- car-app/src/app/components/search-bar/search-bar.component.ts
+++ car-app/src/app/components/search-bar/search-bar.component.ts
@@ -11,6 +11,7 @@ declare var google;
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  searchFromV: string;
   @ViewChild("searchFrom") searchFromElementRef: ElementRef;
   @ViewChild("searchTo") searchToElementRef: ElementRef;
   @Output() inputChanged: EventEmitter<any> = new EventEmitter<any>();
@@ -38,4 +39,64 @@ export class SearchBarComponent implements OnInit {
     }
     this.inputChanged.emit(form);
   }
+
+  private fromBtn() {
+    setTimeout(() => {
+      this.searchFromV = this.searchFromElementRef.nativeElement.value;
+      console.log(this.searchFromV);
+    }, 0.1);
+  }
 }
+
+
+
+/*
+
+import { Component, OnInit, ElementRef, NgZone, ViewChild, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
+declare var google;
+
+export class SearchBarComponent implements OnInit {
+  searchFromV: string;
+  searchToV: string;
+  fromText: string;
+  @ViewChild("searchFrom") searchFromElementRef: ElementRef;
+  @ViewChild("searchTo") searchToElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      let autocompleteOne = new google.maps.places.Autocomplete(this.searchFromElementRef.nativeElement, {
+        types: ["geocode"]
+      });
+      let autocompleteTwo = new google.maps.places.Autocomplete(this.searchToElementRef.nativeElement, {
+        types: ["geocode"]
+      });
+    });
+  }
+
+
+  private delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  private fromBtn() {
+    setTimeout(() => {
+      this.searchFromV = this.searchFromElementRef.nativeElement.value;
+      console.log(this.searchFromV);
+    }, 0.1);
+  }
+
+  private searchDestination(searchFrom: string, searchTo: string) {
+    console.log("2");
+  }
+}
+
+*/
