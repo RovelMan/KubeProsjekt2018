45d17e715f9041dc46cab539656cc9d184f3efd5
Tor Arne <torarne1996@hotmail.no>
2018-02-13 23:51:53 +0100

    Results component can now use data from search component to search in database and display the correct trips.

--- car-app/src/app/components/results/results.component.ts
+++ car-app/src/app/components/results/results.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, ElementRef, NgZone, Input} from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { TripHandlerService } from '../../services/trip-handler.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent implements OnChanges {
 
-  fromDest: String;
-  toDest: String;
-  numberOfPassengers: Number;
-  date: String;
+  @Input() searchForm: any;
+  resultsArray: any;
 
-  constructor() { }
+  constructor(
+    private tripHandler: TripHandlerService, 
+    private flashMessage: FlashMessagesService
+  ) { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.findTrips(this.searchForm);
+      setTimeout(() => {
+        console.log("Outside", this.resultsArray);
+      }, 100);
+  }
+
+  findTrips(trip: any) {
+    const thisTrip = {
+      from: trip.fromText,
+      to: trip.toText
+    }
+    this.tripHandler.findTripFromDest(thisTrip).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show("You have now found all available trips", { cssClass: 'alert-success', timeout: 3000 });
+        this.resultsArray = data.tripsFound;
+        console.log("Inside", this.resultsArray);
+      } else {
+        this.flashMessage.show("Something went wrong", { cssClass: 'alert-danger', timeout: 3000 });
+      }
+    });
   }
 
 }
